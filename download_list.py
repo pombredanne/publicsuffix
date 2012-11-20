15108e4939282f8588e31bbed7860552168369b8
Richard Boulton <richard@tartarus.org>
2012-11-20 10:15:49 +0000

    Add a script to download the suffix list from mozilla, and store the list in the repository.

---
+++ download_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env pytthon
+
+import os.path
+import urllib
+import publicsuffix
+
+outdir = os.path.dirname(__file__)
+outpath = os.path.join(outdir, 'publicsuffix.txt')
+with file(outpath, 'w') as out_fd:
+    in_fd = urllib.urlopen(publicsuffix.EFFECTIVE_TLD_NAMES)
+    try:
+        data = in_fd.read()
+        out_fd.write(data)
+    finally:
+        in_fd.close()
+print "Downloaded %d bytes, stored in %s" % (len(data), outpath)
